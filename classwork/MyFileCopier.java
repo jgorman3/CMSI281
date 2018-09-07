f59536bb780bd1ee610dddf3e6d769d8fa98b7de
Joe Gorman <jgorman3@lion.lmu.edu>
2018-09-06 17:11:48 -0700

    created myfilecopier.java
    still working on it

---
+++ classwork/MyFileCopier.java
@@ -0,0 +1,35 @@
+ MyFileCopier.java   1.0.0   5-September-2018
+*  Purpose    : Simple Class that reads and writes files
+*  Author     : Joe Gorman
+*  Description: The intent of the exercise is to get experience with
+*               packages, and with having different classes perform
+*               different operations as part of the program's
+*               functionality. The file copier will be command
+*               line or terminal window driven.
+*/
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.lang.StringBuffer;
+import copiersupport.*;
+
+public class MyFileCopier {
+  //class fields
+  public static final int END_OF_FILE = -1;
+  //main constructor
+  public MyFileCopier() {
+    //MyFileCopier filecopier = new MyFileCopier();
+  }
+  //main method
+  public static void main(String[] args) throws IOException{
+    System.out.println("Enter a file name: ");
+    Scanner myFile = new Scanner(System.in);
+    String filename = myFile.nextLine();
+    SourceFile src = new SourceFile(filename);
+    String buffer = src.readtoString(filename);
+    System.out.println(buffer);
+    //TargetFile target = new TargetFile(filename, buffer);
+  }
+}
