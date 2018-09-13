6a168a78be7237fc2ca88325bcde98b03bd0fdf0
Joe Gorman <jgorman3@lion.lmu.edu>
2018-09-13 16:37:40 -0700

    added a header

--- classwork/Classwork03/IntListInterface.java
+++ classwork/Classwork03/IntListInterface.java
@@ -1,3 +1,8 @@
+/** IntListInterface.java   1.0.0   13-September-2018
+*  Purpose    : Simple Interfaces that works with IntList.java
+*  Author     : Joe Gorman
+*  Description: Interface implements four different methods from IntList.java
+*/
 public interface IntListInterface {
       int getValueAtIndex( int index );
       boolean append( int valueToAdd );
