2cb47967cf3c4944b268b9b627be413a66131456
Joe Gorman <jgorman3@lion.lmu.edu>
2018-09-13 12:25:19 -0700

    Create IntListInterface.java

---
+++ classwork/Classwork03/IntListInterface.java
@@ -0,0 +1,6 @@
+public interface IntListInterface {
+      int getValueAtIndex( int index );
+      boolean append( int valueToAdd );
+      boolean insertValueAtIndex( int value, int index );
+      int removeValueAtIndex( int index );
+   }
