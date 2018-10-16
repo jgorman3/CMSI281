0f443a3e9babda42711e92dac2c9318b59b672b6
Joe Gorman <jgorman3@lion.lmu.edu>
2018-10-16 16:41:13 -0700

    Created QueueApp.java

---
+++ Homework/Homework02/QueueApp.java
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////
+class QueueApp
+{
+  public static void main(String[] args)
+  {
+    Queue theQueue = new Queue(5); // queue holds 5 items
+    theQueue.insert(10); // insert 4 items
+    theQueue.insert(20);
+    theQueue.insert(30);
+    theQueue.insert(40);
+    theQueue.remove();   // remove 3 items
+    theQueue.remove();   // (10, 20, 30)
+    theQueue.remove();
+    theQueue.insert(50); // insert 4 more items
+    theQueue.insert(60); // (wraps around)
+    theQueue.insert(70);
+    theQueue.insert(80);
+    theQueue.dispQueue();
+    //while( !theQueue.isEmpty() ) // remove and display
+    //{ // all items
+    //  long n = theQueue.remove(); // (40, 50, 60, 70, 80)
+    //  System.out.print(n);
+    //  System.out.println(' ');
+    //}
+    //System.out.println(' ');
+  } // end main()
+} // end class QueueApp
