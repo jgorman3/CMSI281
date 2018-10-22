c62094df1aa608b983ecc6717b5f6365a39722e1
Joe Gorman <jgorman3@lion.lmu.edu>
2018-10-21 22:38:05 -0700

    made some final changes to code

--- Homework/Homework02/Deque.java
+++ Homework/Homework02/Deque.java
@@ -5,57 +5,82 @@
 // Description : The intent of the this class is to be able to 
 //               access data from the back and front of sequential list
 ////////////////////////////////////////////////////////////////
-class Deque {
+public class Deque {
+    private int maxSize;
+    private int front;
+    private int rear;
+    private int nItems;
+    private long[] dArray;
 
-  private int maxSize;
-  private long[] deqArray;
-  private int front;
-  private int rear;
-  private int nItems;
 
-  public Deque(int s){
-    maxSize = s;
-    deqArray = new long[maxSize];
-    front = 0;
-    rear = -1;
-    nItems = 0;
+    public Deque( int size ) {
+        maxSize = size;
+        front = 0;
+        rear = -1;
+        nItems = 0;
+        dArray = new long[maxSize];
+    }
+
+    //insert at front of array
+    public void insertLeft( long j ) {
+        if ( front == 0 ) {
+            front = maxSize;
+        }
+        dArray[--front] = j;
+        nItems++;
+    }
+
+    //insert at rear of array
+    public void insertRight( long j ) {
+        if ( rear == maxSize - 1 ) {
+            rear = -1;
+        }
+        dArray[++rear] = j;
+        nItems++;
+    }
+
+    //Removes from the front
+    public long removeLeft() {
+        long temp = dArray[front++];
+        if ( front == maxSize ) {
+            front = 0;
+        }
+        nItems--;
+        return temp;
+    }
+
+    //removes item at rear
+    public long removeRight() {
+        long temp = dArray[rear--];
+        if ( rear == -1 ) {
+            rear = maxSize-1;
+        }
+        nItems--;
+        return temp;
+    }
+
+
+    public boolean isEmpty() {
+        return ( nItems == 0 );
+    }
+
+
+    public boolean isFull() {
+        return ( nItems == maxSize );
+    }
+
+
+    public void displayDequeue() {
+        int index = front;
+        for ( int i = 0; i < nItems; i++ ) {
+            System.out.print( dArray[index] + " " );
+            if ( index == maxSize - 1 ) {
+                index = 0;
+            }
+            else {
+                index++;
+            }
+        }
+        System.out.println();
+    }
   }
-  //----------------------------------------------
-  public void insertLeft(long j){
-    if (front == maxSize-1)
-      front = 0;
-    deqArray[front++] = j;
-    nItems++;
-  }
-  //----------------------------------------------
-  public void insertRight(long j){
-    if(rear == maxSize-1) // deal with wraparound
-      rear = -1;
-    deqArray[++rear] = j; // increment rear and insert
-    nItems++; // one more item
-  }
-  //----------------------------------------------
-  public long removeLeft(){
-    long temp = deqArray[front++]; // get value and incr front
-    if(front == maxSize) // deal with wraparound
-      front = 0;
-    nItems--; // one less item
-    return temp;
-  }
-  //----------------------------------------------
-  public long removeRight(){
-    long temp = deqArray[rear++];
-    if (rear == maxSize)
-      rear = -1;
-    nItems--;
-    return temp;
-  }
-  //----------------------------------------------
-  public boolean isEmpty(){
-    return (nItems==0);
-  }
-  //----------------------------------------------
-  public boolean isFull(){
-    return (nItems==maxSize);
-  }
-}
